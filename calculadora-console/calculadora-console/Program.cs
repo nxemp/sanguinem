69bc7a007508de40f9ac05eaaf2fd629606be709
Wallysson Lima <wallyssonlds@gmail.com>
2016-03-24 13:43:54 -0300

    implementando a função para calcular

--- calculadora-console/calculadora-console/Program.cs
+++ calculadora-console/calculadora-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace calculadora_console
 {
@@ -10,6 +7,34 @@ namespace calculadora_console
     {
         static void Main(string[] args)
         {
+            
+            // passar as notas
+            notas(6.0f, 6.0f, 5.9f, 6.0f);
+        }
+
+        public static void notas(float n1, float n2, float n3, float n4)
+        {
+            float nFinal = (2 * n1 + 2 * n2 + 3 * n3 + 3 * n4 ) / 10;
+            
+            if (nFinal < 6.0f)
+            {
+                for (float i = 0.1f; i != 10.0f; i++)
+                {
+                    if (((nFinal + i) / 2) >= 6.0f)
+                    {
+                        MessageBox.Show("Para passar você precisa de " + i + " na prova final.");
+                        break;
+                    }
+                    
+                }
+                
+            }
+            
+            else if (nFinal >= 6) 
+            {
+                MessageBox.Show("Sua média é: " + nFinal + " Você foi aprovado!");
+            }
+          
         }
     }
 }
